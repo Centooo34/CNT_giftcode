310eebb3284c2274b7ef7eb06d180e056de9b411
Cento Scripts <166239193+Centooo34@users.noreply.github.com>
2025-03-20 15:32:30 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -7,7 +7,7 @@ AddEventHandler("giftcode:redeem", function(code)
     local reward = Config.GiftCodes[code]
     
     if not reward then
-        TriggerClientEvent('ox_lib:notify', source, {type = 'error', description = Config.Notify.Failure})
+            TriggerClientEvent('ox_lib:notify', source, {type = 'error', description = Config.Notify.Failure})
         return
     end
     
@@ -22,4 +22,23 @@ AddEventHandler("giftcode:redeem", function(code)
     TriggerClientEvent('ox_lib:notify', source, {type = 'success', description = Config.Notify.Success})
 
     xPlayer.set("usedGiftCode", true) 
+end)
+
+
+Citizen.CreateThread(function()
+    local resourceName = GetCurrentResourceName()
+    local currentVersion = GetResourceMetadata(resourceName, "version")
+
+    PerformHttpRequest("https://raw.githubusercontent.com/Centooo34/cento_giftcode/main/version.txt", function(statusCode, newVersion, headers)
+        if statusCode == 200 then
+            newVersion = newVersion:gsub("\n", "") -- Odstráni nové riadky
+            if currentVersion ~= newVersion then
+                print("^1[WARNING] Your version " .. resourceName .. " (" .. currentVersion .. ") is old! Newest version is " .. newVersion .. ". Download newest version from https://github.com/Centooo34/cento_giftcode/releases.^0")
+            else
+                print("^2[INFO] " .. resourceName .. " is up fo date!^0")
+            end
+        else
+            print("^3[WARNING] Failed to check version of script " .. resourceName .. ".^0")
+        end
+    end, "GET", "", {})
 end)
