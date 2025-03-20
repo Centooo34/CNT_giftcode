fbebd7ab2b98d78696908d98b2e31b105ce561d3
Cento Scripts <166239193+Centooo34@users.noreply.github.com>
2025-03-20 15:19:14 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,10 +2,14 @@ Config = {}
 
 Config.GiftCodes = {
     ["GIFT2023"] = {item = "bread", count = 5}, 
-    -- add more codes
+    -- add more codes and rewards
 }
 
 Config.Notify = {
     Success = "Úspěšně jste obdrželi odměnu!", -- succes notify
     Failure = "Chybný kód! Zkuste to znovu." -- fail notify
+}
+
+Config.Menu = {
+    Input = "Enter your code here",-- text in menu input
 }
